559b12fafd11f82978767de09eb8b00061c52996
JacobEscudero <jacobesc9@gmail.com>
2025-09-25 12:50:11 -0500

    el boton modificar la imagen

--- ejercicioGit/ejercicio.js
+++ ejercicioGit/ejercicio.js
@@ -1,3 +1,9 @@
 saludar=function(){
+    let cmpImagenSaludo
+    cmpImagenSaludo= document.getElementById("imgSaludo");
+    cmpImagenSaludo.src="./imagenes/ganar.gif"
     console.log("ingresa a saludar");
+    
+
+
 }
